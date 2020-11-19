420bc21e1596487892c66d471e3d8234a2470ff7
sankrish <66157644+santhinikrish@users.noreply.github.com>
2020-11-19 14:33:25 +0530

    c add on

--- corner elements-matrix.c
+++ corner elements-matrix.c
@@ -103,3 +103,51 @@ int main()
         printf("\n");
     }
 }
+
+
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,k;
+    scanf("%d\n",&n);
+    int a[n][n];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            scanf("%d ",&a[i][j]);
+        }
+    }
+    scanf("%d",&k);
+    int count=k;
+    for(int i=0;i<n;i++,printf("\n"))
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(j<count || j>=n-count)
+            {
+                printf("%d ",a[i][j]);
+            }
+            else
+            {
+                printf("* ");
+            }
+        }
+        if(i>=n-k)
+        {
+            count++;
+        }
+        else
+        {
+            count--;
+        }
+        if(i==n-k-1)
+        {
+            count=1;
+        }
+    }
+
+
+}
